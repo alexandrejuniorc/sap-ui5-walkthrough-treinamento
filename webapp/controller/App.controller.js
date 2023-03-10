d6077cc2bc7c7532e081e349fa974b7d4bc9e951
Alexandre Junior <alexandrejunior408@gmail.com>
2023-03-10 17:30:15 -0300

    chore: Nexted Views - separa o painel que foi criado em outra view e sua lógica em outro controller para fazer apenas sua chamada na view App

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,25 +1,8 @@
 sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
+  ["sap/ui/core/mvc/Controller"],
   function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
 
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onShowHello: function () {
-        // read message from i18n model
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        var sMessage = oBundle.getText("helloMessage", [sRecipient]);
-
-        // Show message
-        MessageToast.show(sMessage);
-      },
-    });
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {});
   }
 );
