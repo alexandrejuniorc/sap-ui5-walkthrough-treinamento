3df8fa68fc79fa8c504fa063673965fc04fd7094
Alexandre Junior <alexandrejunior408@gmail.com>
2023-03-13 11:01:51 -0300

    chore: Fragment Callbacks - cria botão de fechar dentro do modal, assim mostrando como colocar componentes dentro do modal

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
   ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
-  function (Controller, MessageToast,Fragment) {
+  function (Controller, MessageToast, Fragment) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
@@ -16,26 +16,29 @@ sap.ui.define(
         MessageToast.show(sMessage);
       },
       onOpenDialog: function () {
-        var oView = this.getView()
+        var oView = this.getView();
 
         // create the dialog lazily
-        if(!this.byId("helloDialog")){
+        if (!this.byId("helloDialog")) {
           // load asynchronous XML fragment
 
           Fragment.load({
             id: oView.getId(),
-            name: "sap.ui.demo.walkthrough.view.HelloDialog"
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+            controller: this,
           }).then(function (oDialog) {
             // connect dialog to the root view of this component (models, lifecycle)
 
-            oView.addDependent(oDialog)
-            oDialog.open()
-          })
+            oView.addDependent(oDialog);
+            oDialog.open();
+          });
         } else {
-          this.byId("helloDialog").open()
+          this.byId("helloDialog").open();
         }
-
-      }
+      },
+      onCloseDialog: function () {
+        this.byId("helloDialog").close();
+      },
     });
   }
 );
